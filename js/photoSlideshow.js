792dfae04459bb61c4f41b052ea536cd3c0a794b
chrisdennett <chrisdennett@gmail.com>
2023-03-19 15:10:32 +0000

    got the zoom working.

--- js/photoSlideshow.js
+++ js/photoSlideshow.js
@@ -11,6 +11,7 @@ export class PhotoSlideshow {
     this.ctx.fillRect(0, 0, width, height);
     this.currPhotoIndex = 0;
     this.isSetup = false;
+    this.totalPhotos;
   }
 
   async setup({ photoData, timings, duration }) {
@@ -23,10 +24,10 @@ export class PhotoSlideshow {
       const img = await loadImage(`./img/${p}`);
       this.photos.push(img);
     }
+    this.totalPhotos = this.photos.length;
 
     if (!timings) {
-      const totalPhotos = this.photos.length;
-      this.timings = this.getDefaultTimings(totalPhotos, duration);
+      this.timings = this.getDefaultTimings(this.totalPhotos, duration);
     } else {
       this.timings = timings;
     }
@@ -38,22 +39,73 @@ export class PhotoSlideshow {
 
   update(currTime, duration) {
     if (!this.isSetup) return;
+    const showingLastPhoto = this.currPhotoIndex === this.photos.length - 1;
 
     // at the start or end reset the current photo index
     if (currTime >= duration || currTime === 0) {
       this.currPhotoIndex = 0;
-    } else {
-      const showingLastPhoto = this.currPhotoIndex === this.photos.length - 1;
-      const timeToChangePhoto = showingLastPhoto
-        ? duration
-        : this.timings[this.currPhotoIndex];
-
-      if (currTime >= timeToChangePhoto) {
-        this.currPhotoIndex++;
-      }
     }
 
-    this.ctx.drawImage(this.photos[this.currPhotoIndex], 0, 0);
+    const timeToChangePhoto = showingLastPhoto
+      ? duration
+      : this.timings[this.currPhotoIndex];
+
+    if (currTime >= timeToChangePhoto) {
+      this.currPhotoIndex++;
+    }
+
+    // grab from timings if provided
+    const currImg = this.photos[this.currPhotoIndex];
+    const nextImg =
+      this.currPhotoIndex < this.photos.length - 1
+        ? this.photos[this.currPhotoIndex + 1]
+        : this.photos[0];
+
+    let currImgOpacity = 1;
+    let showNextImage = true;
+
+    // if a second from next change point
+    if (timeToChangePhoto - currTime < 1) {
+      currImgOpacity = timeToChangePhoto - currTime;
+      showNextImage = true;
+    }
+
+    // if a second from start fade in current image from black
+    if (currTime < 1) {
+      currImgOpacity = currTime;
+      showNextImage = false;
+    }
+
+    // if a second from the end fade out current image
+    if (duration - currTime < 1) {
+      currImgOpacity = duration - currTime;
+      showNextImage = false;
+    }
+
+    // zoom int the current image in a linear fashion up to a max zoom
+    const maxScaleIncrease = 0.5;
+    const currPhotoStart =
+      this.currPhotoIndex === 0 ? 0 : this.timings[this.currPhotoIndex - 1];
+
+    const zoomProgress = (currTime - currPhotoStart) / timeToChangePhoto;
+    const currImgScale = 1 + maxScaleIncrease * zoomProgress;
+
+    this.ctx.save();
+    // start by clearing with black
+    this.fillStyle = "#000000";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    // draw the next image behind if needed
+    if (showNextImage) {
+      this.ctx.drawImage(nextImg, 0, 0);
+    }
+
+    this.ctx.globalAlpha = currImgOpacity;
+    this.ctx.translate(this.canvas.width / 2, this.canvas.height / 2);
+    this.ctx.scale(currImgScale, currImgScale);
+    this.ctx.translate(-this.canvas.width / 2, -this.canvas.height / 2);
+    this.ctx.drawImage(currImg, 0, 0);
+    this.ctx.restore();
   }
 
   getDefaultTimings(totalPhotos, duration) {
